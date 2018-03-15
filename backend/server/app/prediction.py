173dbff95e5981ff8bdd25519ae7ac3942506c10
Francisco Matos <franciscomatos@ua.pt>
2018-03-15 14:31:39 +0000

    Atualização das dependências do servidor. Correções e comentários no código do servidor. Web crawler tem métodos para extração de eventos em Aveiro (ainda em desenvolvimento)

--- backend/server/app/prediction.py
+++ backend/server/app/prediction.py
@@ -1,10 +1,22 @@
+'''
+Contém métodos para classificar imagens usando a API do Clarifai
+
+predict_image(filename) -> recebe o path para uma imagem e usa a API do
+Clarifai para a identificar e classificar
+'''
+
 from clarifai.rest import ClarifaiApp
 from clarifai.rest import Image as ClImage
 import json
 
-API_KEY = 'ca151274dc0546cdb191aff8fa00ec17'
+API_KEY = 'ca151274dc0546cdb191aff8fa00ec17' # chave do Bruno
 
 def predict_image(filename):
+    '''
+    Usa a API do Clarifai para identificar o conceito apresentado numa imagem
+
+    filename - path para o ficheiro de imagem a enviar para a API
+    '''
     app = ClarifaiApp(api_key=API_KEY)
 
     model_title = "Universidade de Aveiro"
--- backend/server/app/search.py
+++ backend/server/app/search.py
@@ -1,6 +1,22 @@
+'''
+Contém métodos para pesquisar informação na wikipedia usando
+a wikimedia API
+
+search_wiki(search_field, lang) -> recebe o conceito a pesquisar e
+a linguagem da wikipedia a usar e retorna o resumo da página encontrada
+'''
+
 import wikipedia
 
 def search_wiki(search_field, lang='PT'):
+    '''
+    Usa a API da wikimedia para fazer pesquisas na wikipedia
+    e retorna o resumo do conceito pesquisado
+
+    search-field - string com o termo a pesquisar
+    
+    lang - lingua da wikipedia (default: "PT")
+    '''
     wikipedia.set_lang(lang) # mudar a linguagem para português
 
     try:

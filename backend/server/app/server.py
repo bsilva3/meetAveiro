8a7afd6122b6cf90c138aad18ad8b9d9394f385c
Francisco Matos <franciscomatos@ua.pt>
2018-05-21 16:05:31 +0100

    Correções no endpoint de atrações

--- backend/server/app/server.py
+++ backend/server/app/server.py
@@ -559,13 +559,16 @@ def get_atractions():
             geolocator = Nominatim()
             print(str(c.latitude) + ', ' + str(c.longitude))
             location = geolocator.reverse(str(c.latitude) + ', ' + str(c.longitude))
-            location = location.address.split(',')
-            if len(location) <= 9 and len(location) > 1:
-                temp['city'] = location[2]
-            elif len(location) >= 10:
-                temp['city'] = location[3]
-            else:
+            if location is None:
                 temp['city'] = ''
+            else:
+                location = location.address.split(',')
+                if len(location) <= 9 and len(location) > 1:
+                    temp['city'] = location[2]
+                elif len(location) >= 10:
+                    temp['city'] = location[3]
+                else:
+                    temp['city'] = ''
         fotos = db.session.query(Fotografia).filter(Fotografia.nomeconc==c.nomeconceito)
         f = fotos[0]
         foto = {}

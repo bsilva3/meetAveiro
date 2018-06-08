a6b95ffeef9931f5c49f42eb9dcf30c8cee6a3ce
Francisco Matos <franciscomatos@ua.pt>
2018-06-08 22:49:20 +0100

    Correcoes nas instancias de percurso do user

--- backend/server/app/static/js/routes.js
+++ backend/server/app/static/js/routes.js
@@ -1,20 +1,19 @@
 function share_map_2() {
-    var e = document.getElementById("routeslist");
-    var id = e.options[e.selectedIndex].value;
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", '/resources/routes/user_instances/' + id + '/myinstances', true);
+    
+    //var xhr = new XMLHttpRequest();
+    //xhr.open("POST", '/resources/routes/user_instances/' + id + '/myinstances', true);
 
     //Send the proper header information along with the request
-    xhr.setRequestHeader("Content-type", "application/json");
+    //xhr.setRequestHeader("Content-type", "application/json");
 
-    xhr.onload  = function() {
-        var jsonResponse = JSON.parse(xhr.responseText);
+    //xhr.onload  = function() {
+    //    var jsonResponse = JSON.parse(xhr.responseText);
         // do something with jsonResponse
-        location.href = jsonResponse.url;
-     };
+    //    location.href = jsonResponse.url;
+     //};
 
-    xhr.send(JSON.stringify({
-        topic: concept,
-        method: 'GET'
-    }));
+    //xhr.send(JSON.stringify({
+    //    topic: concept,
+    //    method: 'GET'
+    //}));
 }
